d222da6e3a3af3a13cc3d8a3385350305817e9dc
leechance <workinterface@126.com>
2015-11-30 20:07:40 +0800

    Initial Commit

---
+++ ParabolaMoving/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParabolaMoving
+//
+//  Created by chance on 15/11/30.
+//  Copyright © 2015年 Shanghai Zhongyi Communication Technology Engieering Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParabolaMoving/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParabolaMoving
+//
+//  Created by chance on 15/11/30.
+//  Copyright © 2015年 Shanghai Zhongyi Communication Technology Engieering Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
